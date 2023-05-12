82b1a35249c0e6a19f80b93f4c81fa59de2d2d24
Lucas Crijns <lucascrijns@gmail.com>
2023-05-13 00:56:41 +0200

    Change timeout

--- scripts/summary.py
+++ scripts/summary.py
@@ -5,6 +5,8 @@ from api_interface.login import login_and_get_token
 from api_interface.games import get_most_recent_games_after_time, fetch_game
 from api_interface.game_analysis import analyse_game
 
+WAIT_TIMEOUT = timedelta(minutes=1)
+
 
 def check_recent_games(token, timestamp):
     games = get_most_recent_games_after_time(token, timestamp)
@@ -24,8 +26,8 @@ def main():
     print("Successfully logged in")
 
     while not (game := check_recent_games(token, timestamp)):
-        print("No new games found, waiting 5 seconds")
-        time.sleep(5)
+        print(f"No new games found, waiting {WAIT_TIMEOUT} seconds")
+        time.sleep(WAIT_TIMEOUT.seconds)
 
     print(f"Found game: {game['id']}")
     game_data = fetch_game(token, game["id"])
