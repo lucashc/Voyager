92a817076070ba3183805d9cee0bbbca7f7068c7
Lucas Crijns <lucascrijns@gmail.com>
2023-05-13 21:46:38 +0200

    Update runner script

--- scripts/run_agent.sh
+++ scripts/run_agent.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "Running Image"
-podman run -v ./results:/files:z -v ./:/mnt:z --rm --name agent -it tarasyarema/sqlillo /mnt/main.lua /mnt/main.lua /mnt/main.lua /mnt/main.lua /mnt/hitman.lua /mnt/hitman.lua
+if [[ $# != 2 ]] ; then
+    echo 'Please provide the number of agents to run: ./run_agent.sh <number_of_agents> <number_of_hitmen>'
+    exit -1
+fi
+
+AGENTS=$(seq $1 | awk '{print "/mnt/main.lua"}')
+HITMEN=$(seq $2 | awk '{print "/mnt/hitman.lua"}')
+
+echo "Running Image with $1 agents and $2 hitmen"
+podman run -v ./results:/files:z -v ./:/mnt:z --rm --name agent -it tarasyarema/sqlillo $AGENTS $HITMEN
 
 echo "Rename results"
 mv results/traces.json "results/$(date -Iseconds)_trace.json"
