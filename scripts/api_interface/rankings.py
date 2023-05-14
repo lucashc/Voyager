460223f44035d236f2577d28b3994fba7dc89710
Lucas Crijns <lucascrijns@gmail.com>
2023-05-14 02:02:47 +0200

    Remove TQDM from workflow

--- scripts/api_interface/rankings.py
+++ scripts/api_interface/rankings.py
@@ -1,7 +1,6 @@
 from api_interface.games import get_past_games, fetch_game
 from api_interface.game_analysis import get_players, get_moves, bool_to_checkmark
 from concurrent.futures import ThreadPoolExecutor
-from tqdm import tqdm
 from collections import defaultdict
 
 
@@ -12,10 +11,8 @@ def get_ranking_pretty(token, N_GAMES, progress_disable=True):
     games = []
     print("* Downloading games...")
     with ThreadPoolExecutor(max_workers=10) as executor:
-        for game in tqdm(
-            executor.map(lambda game_obj: fetch_game(token, game_obj["id"]), game_objs),
-            total=N_GAMES,
-            disable=progress_disable,
+        for game in executor.map(
+            lambda game_obj: fetch_game(token, game_obj["id"]), game_objs
         ):
             games.append(game)
     print()
@@ -23,7 +20,7 @@ def get_ranking_pretty(token, N_GAMES, progress_disable=True):
 
     print("* Getting rankings...")
     rankings = defaultdict(lambda: 0)
-    for game in tqdm(games, disable=progress_disable):
+    for game in games:
         players = get_players(game)
         for player in players:
             moves = get_moves(game, player)
